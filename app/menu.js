aa137167f9d874c668d9ecbe3754455cd8d3cfed
Vanessa Naff <vanessa@glympse.com>
2018-10-28 23:57:04 -0700

    Genereate -> Generate (typo)

--- app/menu.js
+++ app/menu.js
@@ -29,7 +29,6 @@ export default class MenuBuilder {
     }
 
     setupDevelopmentEnvironment() {
-        this.mainWindow.openDevTools();
         this.mainWindow.webContents.on('context-menu', (e, props) => {
             const { x, y } = props;
 
@@ -98,7 +97,7 @@ export default class MenuBuilder {
                 },
                 { type: 'separator' },
                 {
-                    label: 'Genereate PDF Report',
+                    label: 'Generate PDF Report',
                     accelerator: 'Command+Shift+E',
                     click: () => {
                         this.mainWindow.webContents.send('SAVE_PDF')
