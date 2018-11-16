ea7fc582472f610c507c4dfc8eaad08df33d25e8
S. Gallagher <sjgllghr@gmail.com>
2018-11-16 08:49:41 -0800

    add missing step 3 conditionals

--- app/components/Data.js
+++ app/components/Data.js
@@ -54,9 +54,9 @@ class Data {
             let productionStep = {
                 title: "Introduction to Production and Manufacturing",
                 content: ["https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/develop/app/content/step3-production-manufacturing.md"],
-                questions: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditionalOverhaul/app/assets/quiz/guiding_questions3.json",
-                questionsUI: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditionalOverhaul/app/assets/quiz/guiding_questions3_ui.json",
-                questionRules: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditionalOverhaul/app/assets/quiz/guiding_questions3_rules.json",
+                questions: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditional-2-dreams-come-true/app/assets/quiz/guiding_questions3.json",
+                questionsUI: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditional-2-dreams-come-true/app/assets/quiz/guiding_questions3_ui.json",
+                questionRules: "https://raw.githubusercontent.com/NorthwestGreenChemistry/PrISM/conditional-2-dreams-come-true/app/assets/quiz/guiding_questions3_rules.json",
                 prevStep: "2",
                 nextStep: "4"
             };
